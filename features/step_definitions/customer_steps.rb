f1f054a236977b3a984cc0ad7b048a7e5c56cb85
Chesh <chesh@chesh.info>
2019-05-07 10:47:29 +0000

    fixed mongo

--- features/step_definitions/customer_steps.rb
+++ features/step_definitions/customer_steps.rb
@@ -1,5 +1,5 @@
 Given("a web browser is at the home page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit root_path
 end
 
 When("a user selects a Customer link") do
